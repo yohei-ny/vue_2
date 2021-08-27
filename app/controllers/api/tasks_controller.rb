1ded3ea166de39c0ed5f63f99f4e45c36cc178c3
ny-yohei <yo0314baseball@gmail.com>
2021-08-27 10:23:39 +0900

    apiでのjson

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -1,2 +1,28 @@
 class Api::TasksController < ApplicationController
+  skip_before_action :verify_authenticity_token
+    def index
+      @tasks = Task.order('created_at DESC')
+    end
+
+    def create
+      @task = Task.new(task_params)
+
+      if @task.save
+        render json: @task, status: :created
+      else
+        render json: @task.errors, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      Task.find(params[:id]).destroy!
+    end
+
+    def update
+      Task.find(params[:id]).toggle!(:is_done)
+    end
+
+    private def task_params
+      params.require(:task).permit(:name, :is_done)
+    end
 end
