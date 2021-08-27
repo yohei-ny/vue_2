2d197206cd804a0c406aa9f992ccceb7265fe3a4
ny-yohei <yo0314baseball@gmail.com>
2021-08-27 10:18:23 +0900

    apiコントローラ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'homes#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, format: 'json' do
+    resources :tasks, only: [:index, :create, :destroy, :update]
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_08_27_011710) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.boolean "is_done"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
